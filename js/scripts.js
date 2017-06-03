13565b9456ce432540a319019899147d2af50cbd
jtblog <jtob91@yahoo.com>
2017-06-03 23:31:42 +0100

    C

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+var ws = new WebSocket('wss://ws.binaryws.com/websockets/v3?app_id=1089');
+
+ws.onopen = function(evt) {
+    ws.send(JSON.stringify({ticks:'R_100'}));
+};
+
+ws.onmessage = function(msg) {
+   var data = JSON.parse(msg.data);
+   alert(JSON.stringify(data));
+   //console.log('ticks update: %o', data);
+};
