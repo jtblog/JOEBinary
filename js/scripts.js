b330dc8ce96221728dcab3e2203d9e7ea4e9bda8
jtblog <jtob91@yahoo.com>
2017-06-04 15:25:52 +0100

    C

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,27 @@
-var ws = new WebSocket('wss://ws.binaryws.com/websockets/v3?app_id=1089');
+var ws = new WebSocket('wss://ws.binaryws.com/websockets/v3?app_id=3956');
+
+//var ws = new WebSocket('wss://ws.binaryws.com/websockets/v3?app_id=3956');
 
 ws.onopen = function(evt) {
-    ws.send(JSON.stringify({ticks:'R_100'}));
+    ws.send(JSON.stringify({authorize:'1aStI5HCcty55Ly'}));
+	//ws.send(JSON.stringify({authorize:'1aStI5HCcty55Ly10'}));
 };
 
 ws.onmessage = function(msg) {
    var data = JSON.parse(msg.data);
-   alert(JSON.stringify(data));
-   //console.log('ticks update: %o', data);
+   
+   var strresp = JSON.stringify(data);
+   
+   if(~strresp.indexOf("The token is invalid")){
+	   alert("Invalid tokin");
+   }else{
+	   alert(strresp);
+	   ws.send(JSON.stringify({authorize:'1aStI5HCcty55Ly10'}));
+	  // if(~strresp.indexOf("loginid")){
+		  // a().send(JSON.stringify({ticks:'R_100'}));
+	   //}else{
+		//   alert(strresp);
+	  // }
+	   //var loginid = data.authorize.loginid;
+   }
 };
